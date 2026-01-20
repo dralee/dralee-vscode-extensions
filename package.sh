4c76d11972d24e714eb3b1566505360541c4ae0f
dralee <zhzl_s@126.com>
2026-01-20 14:01:58 +0800

    feat: remove dist

--- package.sh
+++ package.sh
@@ -11,39 +11,40 @@ if [[ "$opt" =~ ^[y|Y]$ ]]; then
     echo after packing it, auto to install
 fi
 
+mkdir -p dist
 cd enum-config-syntax
-vsce package
+vsce package -o ../dist
 $INSTALL && code --uninstall-extension dralee.dralee-enum-config-syntax
-$INSTALL && code --install-extension dralee-enum-config-syntax-0.0.1.vsix
+$INSTALL && code --install-extension ../dist/dralee-enum-config-syntax-0.0.1.vsix
 
 cd ../generator-rule-syntax
-vsce package
+vsce package -o ../dist
 $INSTALL && code --uninstall-extension dralee.dralee-generator-rule-syntax
-$INSTALL && code --install-extension dralee-generator-rule-syntax-0.0.1.vsix
+$INSTALL && code --install-extension ../dist/dralee-generator-rule-syntax-0.0.1.vsix
 
 cd ../export-query-syntax
-vsce package
+vsce package -o ../dist
 $INSTALL && code --uninstall-extension dralee.dralee-export-query-syntax
-$INSTALL && code --install-extension dralee-export-query-syntax-0.0.1.vsix
+$INSTALL && code --install-extension ../dist/dralee-export-query-syntax-0.0.1.vsix
 
 cd ../file-replace-rule-syntax
-vsce package
+vsce package -o ../dist
 $INSTALL && code --uninstall-extension dralee.dralee-file-replace-rule-syntax
-$INSTALL && code --install-extension dralee-file-replace-rule-syntax-0.0.1.vsix
+$INSTALL && code --install-extension ../dist/dralee-file-replace-rule-syntax-0.0.1.vsix
 
 cd ../replace-config-syntax
-vsce package
+vsce package -o ../dist
 $INSTALL && code --uninstall-extension dralee.dralee-content-replace-config-syntax
-$INSTALL && code --install-extension dralee-content-replace-config-syntax-0.0.1.vsix
+$INSTALL && code --install-extension ../dist/dralee-content-replace-config-syntax-0.0.1.vsix
 
 cd ../key-value-config-syntax
-vsce package
+vsce package -o ../dist
 $INSTALL && code --uninstall-extension dralee.dralee-key-value-config-syntax
-$INSTALL && code --install-extension dralee-key-value-config-syntax-0.0.1.vsix
+$INSTALL && code --install-extension ../dist/dralee-key-value-config-syntax-0.0.1.vsix
 
 cd ../line-split-config-syntax
-vsce package
+vsce package -o ../dist
 $INSTALL && code --uninstall-extension dralee.dralee-line-split-config-syntax
-$INSTALL && code --install-extension dralee-line-split-config-syntax-0.0.1.vsix
+$INSTALL && code --install-extension ../dist/dralee-line-split-config-syntax-0.0.1.vsix
 
 cd ..
