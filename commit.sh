b85670ca7ac045b4b274820a7ae21dd7315789de
dralee <zhzl_s@126.com>
2026-01-24 10:15:53 +0800

    feat: line split config param highlight release

--- commit.sh
+++ commit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # 提交并设置相关.sh为可执行
 # by dralee 2025.7.2
+# $2: y/Y for release
 comment=$1
+release=$2
 if [ -z "$comment" ]; then
     comment="feat: update the project"
 fi
@@ -16,3 +18,12 @@ git status
 
 git commit -m "$comment"
 git push
+
+if [[ "$release" =~ ^[yY]$ ]]; then
+    git checkout release
+    git merge --no-ff -m "merge dev to release" dev
+    git push
+
+    echo back to dev for local
+    git checkout dev
+fi
